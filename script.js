c264989891c3f71ba2f2e006669e12391197b32f
troy71 <40522525+troy71@users.noreply.github.com>
2025-05-04 10:42:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-document.querySelector('.hamburger-menu').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('active');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navLinks = document.querySelector('.nav-links');
+const menuItems = document.querySelectorAll('.nav-links a'); // Select all menu links
+
+// Toggle the menu when the hamburger button is clicked
+hamburgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
 });
 
+// Close the menu when a menu item is clicked
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+    });
+});
